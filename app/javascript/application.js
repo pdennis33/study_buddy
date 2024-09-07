35f7d0b10b68daee56b9a2d19cb9cd409fba32d9
Paul Dennis <paul.dennis@hotmail.com>
2024-09-07 12:45:56 -0500

    Clean up unused files, remove UJS and use turbo

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,28 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import Rails from "@rails/ujs";
-Rails.start();
+import "@hotwired/turbo-rails";
+
+// Handle confirmation for links
+document.addEventListener("turbo:click", function(event) {
+    let element = event.target.closest("[data-confirm]");
+    if (element) {
+        let message = element.getAttribute("data-confirm");
+        if (!confirm(message)) {
+            event.preventDefault();  // Prevent the navigation if not confirmed
+        }
+    }
+});
+
+// Handle confirmation for forms (like button_to)
+document.addEventListener("turbo:submit-start", function(event) {
+    let form = event.target;
+    let submitButton = form.querySelector("[data-confirm]");
+
+    if (submitButton) {
+        let message = submitButton.getAttribute("data-confirm");
+
+        if (!confirm(message)) {
+            event.preventDefault();
+            event.detail.formSubmission.stop();
+        }
+    }
+});
