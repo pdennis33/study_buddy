5d8bef347a2c0d83eace24223c1e515a05607131
Paul Dennis <paul.dennis@hotmail.com>
2025-03-04 08:06:13 -0600

    Add honeypot and rack-attack

--- app/controllers/contact_messages_controller.rb
+++ app/controllers/contact_messages_controller.rb
@@ -4,6 +4,12 @@ class ContactMessagesController < ApplicationController
   end
 
   def create
+    if params[:contact_message][:nickname].present?
+      Rails.logger.warn("Spam detected via honeypot!")
+      redirect_to new_contact_message_path
+      return
+    end
+
     @contact_message = ContactMessage.new(contact_message_params)
     if @contact_message.save
       ContactMailer.admin_notification(@contact_message).deliver_now if Rails.env.production?
@@ -11,6 +17,19 @@ class ContactMessagesController < ApplicationController
     else
       render :new
     end
+    #
+    # if verify_recaptcha(action: 'contact_form', minimum_score: 0.5)
+    #   @contact_message = ContactMessage.new(contact_message_params)
+    #   if @contact_message.save
+    #     ContactMailer.admin_notification(@contact_message).deliver_now if Rails.env.production?
+    #     redirect_to new_contact_message_path, notice: "Thank you for your message! While I may not be able to reply, I do read all submissions."
+    #   else
+    #     render :new
+    #   end
+    # else
+    #   Rails.logger.warn("Spam detected via reCAPTCHA!")
+    #   redirect_to new_contact_message_path
+    # end
   end
 
   private
