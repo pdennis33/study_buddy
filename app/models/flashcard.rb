02d1806478efa53e375d74d0f301153a0557eae0
pdennis33 <paul.dennis@hotmail.com>
2025-06-15 14:34:00 -0500

    Set sequence index for new flashcards

--- app/models/flashcard.rb
+++ app/models/flashcard.rb
@@ -2,4 +2,12 @@ class Flashcard < ApplicationRecord
   belongs_to :topic
 
   validates :question, :answer, presence: true
+
+  before_create :set_sequence_index
+
+  private
+
+  def set_sequence_index
+    self.sequence_index = (topic.flashcards.maximum(:sequence_index) || 0) + 1
+  end
 end
